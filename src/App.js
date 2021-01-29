129b83c846146c331092173a6623c2f014541391
Alex Hu <xindi_hu@yahoo.com>
2021-01-29 17:58:41 -0600

    adding text to speech function,finished ver-1

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import CardContainer from "./components/CardContainer";
-import SearchBox from './components/SearchBox.jsx'
-import Scroll from './components/Scroll.jsx'
-
-
+import SearchBox from "./components/SearchBox.jsx";
+import Scroll from "./components/Scroll.jsx";
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       cats: [],
-      searchfield: ''
-    }
+      searchfield: "",
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res=>res.json())
-    .then(users=>this.setState({cats:users}))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ cats: users }));
   }
 
+  onSearchChange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
 
-  onSearchChange=(e)=> {
-    this.setState({ searchfield:e.target.value })
+  render() {
+    const filteredCat = this.state.cats.filter((cats) => {
+      return cats.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+
+    return !this.state.cats.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="App tc">
+        <h1 className="title white">Crazy Kittens</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <p
+          style={{
+            background: "white",
+            borderRadius: "25px",
+            padding: "15px",
+            margin: "55px",
+          }}
+        >
+          type in your letters to filter the kittens,until only one left
+        </p>
+        <Scroll>
+          <CardContainer cats={filteredCat} />
+        </Scroll>
+      </div>
+    );
   }
-
-  render(){
-    const filteredCat = this.state.cats.filter(cats =>{
-      return cats.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-
-    return (!this.state.cats.length)? <h1>Loading</h1> :
-           (
-              <div className="App tc">
-                <h1 className='title white'>Crazy Kittens</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                      <p style={{background:'white',borderRadius:'25px',padding:'15px',margin:'55px'}}>
-                  type in your letters to filter the kittens,until only one left
-                </p> 
-                <Scroll>
-                  <CardContainer cats={filteredCat} />
-                </Scroll>
-          
-              </div>
-            );       
-  } 
 }
 
 export default App;
--- src/components/TellMe.jsx
+++ src/components/TellMe.jsx
@@ -1,25 +1,48 @@
-import React, {useState}from 'react'
-import './tellme.css'
+import React, { useState } from "react";
+import "./tellme.css";
+import { useSpeechSynthesis } from "react-speech-kit";
 
 const TellMe = () => {
-    const [text,setText] = useState("")
+  const [text, setText] = useState("");
+  const { speak } = useSpeechSynthesis();
 
-    const onClick=()=>{
-       fetch('https://v2.jokeapi.dev/joke/Dark,Spooky?blacklistFlags=nsfw,religious,political,racist,sexist')
-       .then(res=>res.json())
-       .then(data=>setText(`${data.setup}${data.delivery} `));        
-    }
-    
-    return (
-        <div className=' tell-me tc dib br3 bg-light-green' >
-            <img className='dib f3'
-            src= "https://www.pinclipart.com/picdir/big/562-5622317_crystal-ball-emoji-png-emoji-crystal-ball-png.png" alt="ball" 
-            style={{height:'300px',width:'auto',padding:'15px'}}   
-            />
-            <button className='shadow-5' onClick={onClick}>click me</button>
-            <p>{text.includes('undefined')? "Let me think.... Ahh, click again": text}</p>
-        </div>
+  const onClick = () => {
+    fetch(
+      "https://v2.jokeapi.dev/joke/Dark,Spooky?blacklistFlags=nsfw,religious,political,racist,sexist"
     )
-}
+      .then((res) => res.json())
+      .then((data) => {
+        data.type === "single"
+          ? setText(`${data.joke}`)
+          : setText(`${data.setup}${data.delivery}`);
+      });
+  };
+
+  const tellMe = () => {
+    speak({ text: text });
+  };
+
+  return (
+    <div className=" tell-me tc dib br3 bg-light-green">
+      <img
+        className="dib f3"
+        src="https://www.pinclipart.com/picdir/big/562-5622317_crystal-ball-emoji-png-emoji-crystal-ball-png.png"
+        alt="ball"
+        style={{ height: "200px", width: "auto", padding: "15px" }}
+      />
+      <div className="btn-container">
+        <button className="shadow-5" onClick={onClick}>
+          click me
+        </button>
+        <button onClick={tellMe}>Speak!!!</button>
+      </div>
+      <p>
+        {text.includes("undefined")
+          ? "Let me think.... Ahh, click again"
+          : text}
+      </p>
+    </div>
+  );
+};
 
 export default TellMe;
