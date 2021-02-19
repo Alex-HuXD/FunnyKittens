5530dd4b6b34d6ea9805ed6e489cd5259a5b7405
Alex Hu <xindi_hu@yahoo.com>
2021-02-19 16:16:34 -0600

    change to hooks from class

--- src/App.js
+++ src/App.js
@@ -1,56 +1,60 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardContainer from "./components/CardContainer";
 import SearchBox from "./components/SearchBox.jsx";
 import Scroll from "./components/Scroll.jsx";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cats: [],
-      searchfield: "",
-    };
-  }
+const App = () => {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     cats: [],
+  //     searchfield: "",
+  //   };
+  // }
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((users) => this.setState({ cats: users }));
+  // }
+  const [cats, setCats] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((users) => this.setState({ cats: users }));
-  }
+      .then((users) => setCats(users));
+  }, []);
 
-  onSearchChange = (e) => {
-    this.setState({ searchfield: e.target.value });
+  const onSearchChange = (e) => {
+    setSearchfield(e.target.value);
   };
 
-  render() {
-    const filteredCat = this.state.cats.filter((cats) => {
-      return cats.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
-    });
+  const filteredCat = cats.filter((cats) => {
+    return cats.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
 
-    return !this.state.cats.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="App tc">
-        <h1 className="title white">Crazy Kittens</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <p
-          style={{
-            background: "white",
-            borderRadius: "25px",
-            padding: "15px",
-            margin: "55px",
-          }}
-        >
-          type in your letters to filter the kittens,until only one left
-        </p>
-        <Scroll>
-          <CardContainer cats={filteredCat} />
-        </Scroll>
-      </div>
-    );
-  }
-}
+  return !cats.length ? (
+    <h1 style={{ color: "white" }}>Loading</h1>
+  ) : (
+    <div className="App tc">
+      <h1 className="title white">Crazy Kittens</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <p
+        style={{
+          background: "darkgreen",
+          borderRadius: "25px",
+          padding: "15px",
+          margin: "55px",
+        }}
+      >
+        type in your letters to filter the kittens,until only one left
+      </p>
+      <Scroll>
+        <CardContainer cats={filteredCat} />
+      </Scroll>
+    </div>
+  );
+};
 
 export default App;
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React from "react";
 
-
-function SearchBox({ searchfield,searchChange}) {
-    return (
-        <div className='searchbox tc pa2'>
-            <input 
-            className='pa3 ba b--green bg-light-blue br3' 
-            type='search' 
-            placeholder='type in your letters' 
-            onChange={searchChange}> 
-            </input>
-        </div>
-    )
-    
+function SearchBox({ searchfield, searchChange }) {
+  return (
+    <div className="searchbox tc pa2">
+      <input
+        className="pa3 ba b--green bg-white br3"
+        type="search"
+        placeholder="type in your letters"
+        onChange={searchChange}
+      ></input>
+    </div>
+  );
 }
 
 export default SearchBox;
